97dd23767081e033c9a2d95206ddb116839c69f4
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-06 10:16:44 +0530

    Add operate function to call math functions

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let num1;
+let num2;
+let operator;
+let answer;
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            answer = add (num1, num2);
+            break;
+        case "-":
+            answer = subtract (num1, num2);
+            break;
+        case "*":
+            answer = multiply (num1, num2);
+            break;
+        case "/":
+            answer = divide (num1, num2);
+            break;
+    }
+}
+
 function add (a, b) {
     return a + b;
 }
