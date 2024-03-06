22f60b070deda93a8a896074af4426bf3f79a42c
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-06 11:17:49 +0530

    Add events to button clicks

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ let num2;
 let operator;
 let answer;
 
+const display = document.querySelector('#display');
+
+const numbersBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('#equal');
+const clearBtn = document.querySelector('#clear');
+
+numbersBtns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.id);
+    })
+})
+
+operatorBtns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.id);
+    })
+})
+
+equalBtn.addEventListener('click', (e) => {
+    console.log(e.target.id);
+})
+
+clearBtn.addEventListener('click', (e) => {
+    console.log(e.target.id);
+})
+
 function operate (num1, num2, operator) {
     switch (operator) {
         case "+":
